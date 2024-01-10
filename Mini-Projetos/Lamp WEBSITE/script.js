0bd1631a404f3c949e398e9a67321dde8cd89e24
NanDev <renandatarde@gmail.com>
2024-01-10 16:58:30 -0300

    commit Lamp
    Finalmente consegui finalizar, só consegui terminar porque precisei rever alguns conteúdos e praticar um pouco mais as condições.

--- Mini-Projetos/Lamp WEBSITE/script.js
+++ Mini-Projetos/Lamp WEBSITE/script.js
@@ -1,43 +1,53 @@
-var botao = 'ligar'
-var lamp = null
-var img = document.getElementById('lamp')
-if (lamp != 'quebrado' || botao != 'quebrado'){
-    function clicaron(){
-        var lamp = 'ligar'
-        if (lamp == botao && lamo != 'quebrado'){
-            window.alert('[ERRO] Já está ligada!')
-        } else{
-            img.src = 'ligada.jpg'
-            botao = 'ligar'
-        }
+let img = document.getElementById('lamp')
+let lamp = 0
+function clicaron() {
+    if (lamp == 1) {
+        alert('Já está ligada!')
+    } else if (lamp == 2) {
+        alert('...')
+        lamp = 2
+    } else {
+        img.src = 'ligada.jpg'
+        lamp = 1
     }
 }
-if (lamp != 'quebrado' || botao != 'quebrado'){
-    function clicaroff(){
-        var  lamp = 'desligar'
-        if (lamp == botao && lamp != 'quebrado'){
-            window.alert('[ERRO] Já está desligada')
-        } else{
-            img.src = 'desligada.jpg'
-            botao = 'desligar'
-        }
-    }
-}
-function quebrar(){
-    var lamp = 'quebrado'
-    var botao = 'quebrado'
-    img.src = 'quebrada.jpg'
-    if (lamp == 'quebrado'){
-        // nada ocorre
-        window.alert('Quebrado...')
-    }
-}   
 
-function sobre(){
-    var lamp = 'ligar'
-    img.src = 'ligada.jpg'
+function clicaroff() {
+    if (lamp == 0) {
+        alert('Já está desligada!')
+    } else if (lamp == 2) {
+        alert('...')
+        lamp = 2
+    } else {
+        img.src = 'desligada.jpg'
+        lamp = 0
+    }
 }
-function sair(){
-    var lamp = 'desligar'
-    img.src = 'desligada.jpg'
+
+function quebrar() {
+    if (lamp == 2) {
+        alert('Nada acontece...')
+    } else {
+        alert('!!!')
+        img.src = 'quebrada.jpg'
+        lamp = 2
+    }
+}
+
+function sobre() {
+    if (lamp == 2) {
+        alert('Nada mais funciona...')
+    } else {
+        img.src = 'ligada.jpg'
+        lamp = 1
+    }
+}
+
+function sair() {
+    if (lamp == 2) {
+        alert('Recarregue a página!')
+    } else {
+        img.src = 'desligada.jpg'
+        lamp = 0 
+    }
 }
